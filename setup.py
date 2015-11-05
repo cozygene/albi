e6dd27fdc3c954725fae35a4eb2d49ed3633d5c4
yedidimr <yedidimr@gmail.com>
2015-11-05 15:29:55 +0200

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+      name='ALBI',
+      version='1.0',
+      url = "https://github.com/cozygene/albi/blob/master/main.py",
+      description = '',
+      scripts=['main.py'],
+    )
