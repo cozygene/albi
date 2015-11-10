13999727402b3a6d520166f2dbf6377dea7b5521
yedidimr <yedidimr@gmail.com>
2015-11-10 17:37:33 +0200

    Rename

--- albi/__init__.py
+++ albi/__init__.py
@@ -1,6 +1,6 @@
 #those three allow to rum "from albi import run" or "from albi import buildHeritability" and use buildHeritability(...)
-from main import run_albi as run
-from albi import build_heritability_cis as buildHeritability
-from albi import calculate_probability_intervals as calcCI
+from albi import run_albi as run
+from albi_lib import build_heritability_cis as buildHeritability
+from albi_lib import calculate_probability_intervals as calcCI
 
 __doc__="TODO add help for the user (is shown on help(albi))"
