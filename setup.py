8d22b78343e68757f4945570bdacfe1741a701ef
Regev Schweiger <regevs@gmail.com>
2017-04-21 16:47:11 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
       version='1.0',
       url = 'https://github.com/cozygene/albi',
       description = '',
-      packages=['albi_lib', 'fiesta_lib'],
+      packages=['albi_lib'],
       scripts=['albi_lib/fiesta.py', 'albi_lib/albi.py', 'albi_lib/progress_bar.py'],
     )
